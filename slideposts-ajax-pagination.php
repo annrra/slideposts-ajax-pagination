ead779ab230f106f50d6e4f7dc6b3feeffa08ed7
andrey <annrra@gmail.com>
2017-03-14 20:20:27 +0200

    update - gallery layout is included

--- slideposts-ajax-pagination.php
+++ slideposts-ajax-pagination.php
@@ -31,16 +31,18 @@ function slideposts_options_do_page() {
 			<?php settings_fields('slide_posts_options'); ?>
 			<?php $options = get_option('sp_sample'); ?>
 			<table class="form-table">
-				<tr valign="top"><th scope="row">Convert the posts into gallery</th>
+				<tr valign="top"><th scope="row">Gallery layout</th>
 					<td>
-                        <input name="sp_sample[option1]" type="checkbox" value="1" <?php checked('1', $options['option1']); ?> /><br />
-                        <p>This part is in progress yet ...</p>
+                        <input name="sp_sample[gallery]" type="checkbox" value="1" <?php checked('1', $options['gallery']); ?> /><br />
+                        <p>Convert the posts into gallery.<br />
+                        As to look good and match the planned layout it is best to show such number of posts per page that their number is divisible by 3 - e.g. 3, 6, 9, 12 ...<br />
+                        It is set in "Posts per page" field</p>
                     </td>
 				</tr>
 				<tr valign="top"><th scope="row">Category name</th>
 					<td><input type="text" name="sp_sample[catname]" value="<?php echo $options['catname']; ?>" /></td>
 				</tr>
-                <tr valign="top"><th scope="row">Posts per age</th>
+                <tr valign="top"><th scope="row">Posts per page</th>
 					<td><input type="text" name="sp_sample[postsnumber]" value="<?php echo $options['postsnumber']; ?>" /></td>
 				</tr>
 			</table>
@@ -54,9 +56,6 @@ function slideposts_options_do_page() {
 
 // Sanitize and validate input. Accepts an array, return a sanitized array.
 function slideposts_options_validate($input) {
-	// Our first value is either 0 or 1
-	$input['option1'] = ( $input['option1'] == 1 ? 1 : 0 );
-	
 	// Say our second option must be safe text with no HTML tags
 	$input['catname'] =  wp_filter_nohtml_kses($input['catname']);
     $input['postsnumber'] =  wp_filter_nohtml_kses($input['postsnumber']);
@@ -88,6 +87,7 @@ function slidepost_ajax_pagination() {
     $options = get_option('sp_sample');
     $cat_name = $options['catname'];
     $postsnum = $options['postsnumber'];
+    $ifgallery = $options['gallery'];
     
         $paged = $_POST['page'];
         $args = array( 'category_name' => $cat_name, 'posts_per_page' => $postsnum, 'paged' => $paged ); 
@@ -97,19 +97,29 @@ function slidepost_ajax_pagination() {
         <div class="slidePostsContainer">
             <div class="slidePostsTab">
             <?php
+            $count=0;
             while ($wp_query->have_posts()) : $wp_query->the_post(); 
             ?>
-                <div class="postBlock">
-                    <h3><?php the_title(); ?></h3>
+                <div class="postBlock postBlock<?php echo $count; ?>">
+                    <?php 
+                    if ($ifgallery == 1) { ?>
                     <div class="postImg">
-                        <?php 
-                        if ( has_post_thumbnail() ) {
-                        	the_post_thumbnail('large');
-                        } ?>
+                        <a class="postEntryLink" href="<?php the_permalink(); ?>"><div class="postOverContainer"><h3><?php the_title(); ?></h3><?php if ( has_post_thumbnail() ) {the_post_thumbnail('thumbnail');} ?><span class="overLay"></span></div></a>
+                    </div>
+                    <?php
+                    } else { ?>
+                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                    <div class="postImg">
+                        <?php if ( has_post_thumbnail() ) {the_post_thumbnail('large');} ?>
                     </div>
                     <div class="postExcerpt"><?php the_content(); ?></div>
+                    <?php
+                    }
+                    ?>
                 </div>
-            <?php endwhile; ?>
+            <?php 
+            $count++;
+            endwhile; ?>
             </div>
             <div class="slidePostsNav">
                 <?php
@@ -138,8 +148,14 @@ function init_listitems( $atts ) {
     $options = get_option('sp_sample');
     $cat_name = $options['catname'];
     $postsnum = $options['postsnumber'];
+    $ifgallery = $options['gallery'];
+    if ($ifgallery == 1) {
+        $wrapmode = "wrapSlideGallery";
+    } else {
+        $wrapmode = "wrapSlideList";
+    }
     
-    $output = '<div class="wrapSlidePosts">'; 
+    $output = '<div class="wrapSlidePosts '. $wrapmode .'">'; 
             
             $paged = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
             $args = array( 'category_name' => $cat_name, 'posts_per_page' => $postsnum, 'paged' => $paged ); 
@@ -150,18 +166,24 @@ function init_listitems( $atts ) {
             $output .= '<div class="pagesNumber">' . $total_pages . '</div>';
             
             $output .= '<div class="slidePostsContainer"><div class="slidePostsTab">';
+                $count=0;
                 while ($wp_query->have_posts()) : $wp_query->the_post(); 
                     
-                    $output .= '<div class="postBlock">';
-                    $output .= '<h3>' . get_the_title() . '</h3>';
-                    $output .= '<div class="postImg">';
-                        if ( has_post_thumbnail() ) {
-                        	$output .= get_the_post_thumbnail($post->ID);
-                        } 
+                    $output .= '<div class="postBlock postBlock'. $count .'">';
+                    if ($ifgallery == 1) {
+                        $output .= '<div class="postImg"><a class="postEntryLink" href="'. get_the_permalink() .'">';
+                        $output .= '<div class="postOverContainer"><h3>' . get_the_title() . '</h3>';
+                        if ( has_post_thumbnail() ) { $output .= get_the_post_thumbnail($post->ID, 'thumbnail'); }
+                        $output .= '<span class="overLay"></span></div></a></div>'; 
+                    } else {
+                        $output .= '<h3><a href="'. get_the_permalink() .'">' . get_the_title() . '</a></h3>';
+                        $output .= '<div class="postImg">';
+                        if ( has_post_thumbnail() ) { $output .= get_the_post_thumbnail($post->ID, 'large'); } 
+                        $output .= '</div>'; 
+                        $output .= '<div class="postExcerpt"><p>' . get_the_content() . '</p></div>';
+                    }
                     $output .= '</div>';
-                    $output .= '<div class="postExcerpt"><p>' . get_the_content() . '</p></div>';
-                    $output .= '</div>';
-    
+                $count++;
                 endwhile;
                 $output .= '</div>';
                 $output .= '<div class="slidePostsNav"><nav class="navigation pagination">';
